dd3608532df4b3d0895020eb1c410d546c8556ef
sstatar <guitar0628393550@gmail.com>
2026-08-21 01:15:52 +0700

    feat: update website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Vocab Tracker",
+  title: "ssitar vocab",
   description: "Track and master your vocabulary",
 };
 
